693c3adcf48267d78612da9053d42062a9ea60b0
calebjoshuapaul <calebjoshuapaul@gmail.com>
2025-02-01 18:22:56 +0530

    Add GitHub Actions workflow for deployment to GitHub Pages and update Next.js configuration for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: "export",
+	images: {
+		unoptimized: true,
+	},
+	basePath:
+		process.env.NODE_ENV === "production" ? "/royalfly-technologies" : "",
 };
 
 export default nextConfig;
